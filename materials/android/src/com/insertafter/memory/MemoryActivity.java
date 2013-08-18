5d342c4250dc295e05cc23de530f0404a283aced
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-08-18 15:42:22 +0200

    Added the Android sources

---
+++ materials/android/src/com/insertafter/memory/MemoryActivity.java
@@ -0,0 +1,35 @@
+package com.insertafter.memory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MemoryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+        
+        WebSettings webSettings = mainWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        
+        mainWebView.setWebViewClient(new MyCustomWebViewClient());
+        mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        
+        mainWebView.loadUrl("http://memory.insertafter.com/index.html");
+    }
+    
+    private class MyCustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
